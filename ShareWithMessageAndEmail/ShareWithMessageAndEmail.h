9b4e3c4fbacb06d26b06085661ce670df538119b
MaYing <609887828@qq.com>
2015-09-16 14:07:20 +0800

    添加

---
+++ ShareWithMessageAndEmail/ShareWithMessageAndEmail.h
@@ -0,0 +1,17 @@
+//
+//  ShareWithMessageAndEmail.h
+//  test
+//
+//  Created by MaYing on 15/9/16.
+//  Copyright (c) 2015年 xmg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MessageUI/MessageUI.h>
+#import <AddressBookUI/AddressBookUI.h>
+
+@interface ShareWithMessageAndEmail : NSObject
+
++(MFMessageComposeViewController *)message:(NSString *)phones msg:(NSString *)message del:(id<MFMessageComposeViewControllerDelegate>)del;//phones 用「,」分割
+-(MFMailComposeViewController *)email:(NSString *)recipient mailSubject:(NSString *)mailSubject mailBody:(NSString *)mailBody isHTML:(BOOL)isHTML del:(id<MFMailComposeViewControllerDelegate> )del;//recipient 收件人 用 ,分割
+@end
---
+++ ShareWithMessageAndEmail/ShareWithMessageAndEmail.m
@@ -0,0 +1,72 @@
+//
+//  ShareWithMessageAndEmail.m
+//  test
+//
+//  Created by MaYing on 15/9/16.
+//  Copyright (c) 2015年 xmg. All rights reserved.
+//
+
+#import "ShareWithMessageAndEmail.h"
+
+@implementation ShareWithMessageAndEmail
++(MFMessageComposeViewController *)message:(NSString *)phones msg:(NSString *)message del:(id<MFMessageComposeViewControllerDelegate>)del
+{
+    if ([MFMessageComposeViewController canSendText])
+    {
+        MFMessageComposeViewController* controller = [[MFMessageComposeViewController alloc] init];
+        NSString * number = phones;//联系人
+        //
+        controller.body =  message;//消息内容
+        NSScanner* numberScanner = [NSScanner scannerWithString:number];
+        [numberScanner setCharactersToBeSkipped:[NSCharacterSet characterSetWithCharactersInString:@",，"]];
+        NSCharacterSet* charactersToSkip = [NSCharacterSet characterSetWithCharactersInString:@",，"];
+        
+        NSMutableArray *substrings = [NSMutableArray array];
+        NSString *substring = @"";
+        while (![numberScanner isAtEnd])
+        {
+            [numberScanner scanUpToCharactersFromSet:charactersToSkip intoString:&substring];
+            [numberScanner scanCharactersFromSet:charactersToSkip intoString:NULL];
+            //NSLog(@"%@", substring);
+            [substrings addObject:substring];
+        }
+        
+        controller.recipients = substrings;
+        controller.messageComposeDelegate = del;
+        return [controller autorelease];
+    }
+    return 0;
+}
+-(MFMailComposeViewController *)email:(NSString *)recipient mailSubject:(NSString *)mailSubject mailBody:(NSString *)mailBody isHTML:(BOOL)isHTML del:(id<MFMailComposeViewControllerDelegate> )del
+{
+    if ([MFMailComposeViewController canSendMail])
+    {
+        MFMailComposeViewController *mailController = [[MFMailComposeViewController alloc] init];
+        
+        NSString * rec = recipient;//收件人
+        //
+        NSScanner* recipientScanner = [NSScanner scannerWithString:rec];
+        [recipientScanner setCharactersToBeSkipped:[NSCharacterSet characterSetWithCharactersInString:@",，"]];
+        NSCharacterSet* charactersToSkip = [NSCharacterSet characterSetWithCharactersInString:@",，"];
+        
+        NSMutableArray *recipients = [NSMutableArray array];
+        NSString *substring = @"";
+        while (![recipientScanner isAtEnd])
+        {
+            [recipientScanner scanUpToCharactersFromSet:charactersToSkip intoString:&substring];
+            [recipientScanner scanCharactersFromSet:charactersToSkip intoString:NULL];
+            //NSLog(@"%@", substring);
+            [recipients addObject:substring];
+        }
+
+        [mailController setToRecipients:recipients];
+        mailController.mailComposeDelegate = del;
+        [mailController setSubject:mailSubject];
+        [mailController setMessageBody:mailBody isHTML:isHTML];
+        mailController.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+        return [mailController autorelease];
+    }
+    return 0;
+}
+
+@end
